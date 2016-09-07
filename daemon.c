d4c2214fbcc87f4d129679243256b87d9d838fe8
Diogo Ferreira <defer@cyngn.com>
2016-09-07 15:00:40 +0100

    su: daemon: Stop leaking child argv
    The daemon builds arguments for child processes but never gets rid of
    them. This patch fixes that.
    
    Change-Id: I9e65ed99e7ba3353553acd532cf4d5ba883669f9

--- daemon.c
+++ daemon.c
@@ -282,6 +282,7 @@ static int daemon_accept(int fd) {
 
     is_daemon = 1;
     int pid = read_int(fd);
+    int child_result;
     ALOGD("remote pid: %d", pid);
     char *pts_slave = read_string(fd);
     ALOGD("remote pts_slave: %s", pts_slave);
@@ -325,6 +326,11 @@ static int daemon_accept(int fd) {
     // is not affected
     int child = fork();
     if (child < 0) {
+        for (i = 0; i < argc; i++) {
+            free(argv[i]);
+        }
+        free(argv);
+
         // fork failed, send a return code and bail out
         PLOGE("unable to fork");
         write(fd, &child, sizeof(int));
@@ -333,6 +339,11 @@ static int daemon_accept(int fd) {
     }
 
     if (child != 0) {
+        for (i = 0; i < argc; i++) {
+            free(argv[i]);
+        }
+        free(argv);
+
         // In parent, wait for the child to exit, and send the exit code
         // across the wire.
         int status, code;
@@ -427,7 +438,12 @@ error:
     }
 #endif
 
-    return run_daemon_child(infd, outfd, errfd, argc, argv);
+    child_result = run_daemon_child(infd, outfd, errfd, argc, argv);
+    for (i = 0; i < argc; i++) {
+        free(argv[i]);
+    }
+    free(argv);
+    return child_result;
 }
 
 int run_daemon() {
