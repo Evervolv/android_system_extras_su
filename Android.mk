1cc71dcacd19f010bfc7229958c02bbbcc4c8541
Nick Reuter <nreuter85@gmail.com>
2016-09-18 23:07:31 -0500

    su: fix LOCAL_CFLAGS
    Change-Id: If12d76855a3a7248f72e3761c2bc6e1cc907a55e

--- Android.mk
+++ Android.mk
@@ -25,27 +25,28 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng debug
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_CFLAGS += -Werror
+
 LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION
 LOCAL_CFLAGS += -DSUPERUSER_EMBEDDED
 LOCAL_CFLAGS += -DREQUESTOR=\"com.evervolv.toolbox\"
 LOCAL_CFLAGS += -DREQUESTOR_PREFIX=\"com.evervolv.toolbox.superuser\"
+LOCAL_CFLAGS += -Werror
 
 LOCAL_INIT_RC := superuser.rc
 
 include $(BUILD_EXECUTABLE)
 
-SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
-$(SYMLINKS): $(LOCAL_MODULE)
-$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+SU_SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
+$(SU_SYMLINKS): $(LOCAL_MODULE)
+$(SU_SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
 	@echo "Symlink: $@ -> /system/xbin/su"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
 	$(hide) ln -sf ../xbin/su $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += $(SU_SYMLINKS)
 
 # We need this so that the installed files could be picked up based on the
 # local module name
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
-    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SU_SYMLINKS)
